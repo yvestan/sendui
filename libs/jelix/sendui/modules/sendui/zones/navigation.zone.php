c9e45a85b7d63d92786a67e0f2fbaa9fe3206d53
Tan <yvestan@gmail.com>
2010-01-21 15:35:12 +0100

    fil d'arianne

---
+++ libs/jelix/sendui/modules/sendui/zones/navigation.zone.php
@@ -0,0 +1,34 @@
+<?php
+ 
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 ff=unix fenc=utf8: */
+
+/**
+ * Zone pour le fil d'arianne
+ *
+ * @package      sendui
+ * @subpackage   sendui
+ * @author       Yves Tannier [grafactory.net]
+ * @copyright    2009 Yves Tannier
+ * @link         http://www.grafactory.net/sendui
+ * @license      http://www.grafactory.net/sendui/licence GPL Gnu Public Licence
+ * @version      0.1.0
+ */
+
+class navigationZone extends jZone {
+ 
+    // templates
+    protected $_tplname='navigation.inc';
+ 
+    // méthode principales
+    protected function _prepareTpl(){
+
+        // parametre du fil d'arianne
+        if($this->getParam('valuesNavigation')) {
+            $valuesNavigation = $this->getParam('valuesNavigation');
+            $this->_tpl->assign('valuesNavigation', $valuesNavigation);
+        }
+
+    }
+
+}
+?>
