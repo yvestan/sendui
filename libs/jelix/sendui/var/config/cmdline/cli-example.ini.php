ee56f656b4b9096c7d7a04e255b40e972003556d
Yves <yvestan@theatre-contemporain.net>
2010-09-03 16:58:29 +0200

    example de bdd conf

---
+++ libs/jelix/sendui/var/config/cmdline/cli-example.ini.php
@@ -0,0 +1,11 @@
+;<?php die(''); ?>
+;for security reasons , don't remove or modify the first line
+
+startModule = "sendui"
+startAction = "sending:index"
+
+; sendui smtp configuration
+[mailerInfo]
+sSmtpUsername="smtp_username"
+sSmtpPassword="smtp_password"
+sSmtpHost="smtp.example.org"
